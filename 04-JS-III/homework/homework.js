679d5d7835df6f8d76a4e2084fa5b2011c7c4a63
Jonathan <jonathan.moran9348@gmail.com>
2022-05-24 18:31:47 -0300

    Realizacion de homework 3

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -186,6 +186,14 @@ function todosIguales(arreglo) {
   //Escriba la función todosIguales, que indique si todos los elementos de un arreglo son iguales:
   //retornar true, caso contrario retornar false.
   //Escribe tu código aquí  
+  var newArray = [];
+
+  for ( i = 0; i < arreglo.lenght; i++){
+    if (newArray [i] === arreglo [i]){
+      return true;
+    }
+  }
+  return false;
   
 } 
 
@@ -195,6 +203,20 @@ function mesesDelAño(array) {
   // "Enero", "Marzo" y "Noviembre", guardarlo en nuevo array y retornarlo.
   //Si alguno de los meses no está, devolver: "No se encontraron los meses pedidos"
   // Tu código:
+  var nuevoArray = [];
+
+  for ( i = 0; i < array.lenght; i++){
+    if ( array [i] === "Enero" || array [i] === "Marzo" || array [i] === "Noviembre"){
+           nuevoArray.push(array [i]);
+    }
+  } 
+    if (array.lenght < 3){
+      return "No se encontraron los meses pedidos";
+    } 
+    else {
+      return nuevoArray;
+    }
+  
 }
 
 
@@ -202,6 +224,15 @@ function mayorACien(array) {
   //La función recibe un array con enteros entre 0 y 200. Recorrer el array y guardar en un nuevo array sólo los
   //valores mayores a 100 (no incluye el 100). Finalmente devolver el nuevo array.
   // Tu código:
+
+  var mayorCien = [];
+
+  for ( i = 0; i < array.lenght; i++){
+    if ( array [i] > 100) {
+      mayorCien.push(array[i]);
+    }
+  }
+  return mayorCien;
 }
 
 
@@ -213,6 +244,23 @@ function breakStatement(numero) {
   //devolver: "Se interrumpió la ejecución"
   //Pista: usá el statement 'break'
   // Tu código:
+  var array = [];
+  var suma = numero;
+
+  for(var i= 0; i<10; i++) {
+    suma = suma + 2;
+    
+    if(suma === i) break;
+    else {
+      array.push(suma);
+    }
+  }
+  if(i < 10) {
+    return 'Se interrumpió la ejecución';
+  }
+  else {
+      return array;
+  }
 }
 
 
@@ -223,7 +271,18 @@ function continueStatement(numero) {
   //Cuando el número de iteraciones alcance el valor 5, no se suma en ese caso y se continua con la siguiente iteración
   //Pista: usá el statement 'continue'
   // Tu código:
+  var array = [];
+  var suma = numero;
+  for(var i= 0; i<10; i++) {
+    if(i === 5) continue;
+    else {
+      suma = suma + 2;
+      array.push(suma);
+    }
+  }
+  return array;
 }
+
 
 
 // No modificar nada debajo de esta línea
